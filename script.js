ee882af0287f6a6101f05f5ca5ba26f3da33f073
personaldropship0-prog <personal.drop.ship0@gmail.com>
2026-01-03 01:23:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Nascondi tutte le sezioni all'inizio
   document.querySelectorAll('.sezione').forEach(s => s.style.display = 'none');
 });
 
 function showSection(id, event) {
-  if(event) event.preventDefault(); // blocca default del pulsante
-  // Nascondi tutte le sezioni
+  if(event) event.preventDefault();
   document.querySelectorAll('.sezione').forEach(s => s.style.display = 'none');
-  // Mostra solo la sezione selezionata
   const sec = document.getElementById(id);
   sec.style.display = 'block';
-  // Scroll direttamente alla sezione
   sec.scrollIntoView({ behavior: 'smooth' });
 }
 
 function goHome() {
-  // Nascondi tutte le sezioni
   document.querySelectorAll('.sezione').forEach(s => s.style.display = 'none');
-  // Scrolla alla Home
   document.getElementById('home').scrollIntoView({ behavior: 'smooth' });
 }
